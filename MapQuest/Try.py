27a355dbe5edd416a6f306d3edd198a3b9153cb7
denverallam <denver.allam.iics@ust.edu.ph>
2021-10-15 17:35:08 +0800

    enhanced

--- MapQuest/Try.py
+++ MapQuest/Try.py
@@ -4,10 +4,11 @@ from tkinter import *
 
 #Create an instance of tkinter window or frame
 win=Tk()
-
+win.resizable(False, False)
+win.title("Map Quest with Gas Calculator")
 orig,dest,gas = "","",""
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
-key = "MhvbvH6lJAKgMu9wisKM5iSoZWOFFPQJ"    # You should use your own key 
+key = "MhvbvH6lJAKgMu9wisKM5iSoZWOFFPQJ"
 
 def get_direction():
         url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
@@ -16,6 +17,7 @@ def get_direction():
         json_data = requests.get(url).json()
         json_status = json_data["info"]["statuscode"]
         if json_status == 0:
+                navigate.destroy()
                 # Label( win, text="API Status: " + str(json_status) + " = A successful route call.\n").pack()
                 Label( win, text="Directions from " + (orig) + " to " + (dest)).pack()
                 Label( win, text="Trip Duration: " + (json_data["route"]["formattedTime"])).pack()
@@ -23,27 +25,25 @@ def get_direction():
                 Label( win, text="Fuel Used (Ltr): " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78))).pack()
                 Label( win, text="Money to be Spent on Fuel: " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78 *int(gas)))).pack()
                 scrollbar.pack(side=RIGHT, fill = Y)
-                myList = Listbox(win,  yscrollcommand=scrollbar.set, width=70)
+                myList = Listbox(win,  yscrollcommand=scrollbar.set, width=100)
                 for each in json_data["route"]["legs"][0]["maneuvers"]:
                         # Label( win, textvariable=narrative).pack()
                         narrative = each["narrative"] + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")
                         myList.insert(END, narrative)
                 myList.pack(side=LEFT, fill=BOTH)
                 scrollbar.config(command=myList.yview)
+                Button(win, height=1, width=10, text="Close ", command=destroy).pack()
+                lbl.destroy()
 
         elif json_status == 402:
-                print("**********************************************")
-                print("Status Code: " + str(json_status) + "; Invalid user inputs for one or bothlocations.")
-                print("**********************************************\n")
+                msg.set("INVALID LOCATION!")
+                lbl.pack()
         elif json_status == 611:
-                print("**********************************************")
-                print("Status Code: " + str(json_status) + "; Missing an entry for one or bothlocations.")
-                print("**********************************************\n")
+                msg.set("INVALID LOCATION!")
+                lbl.pack()
         else:
-                print("************************************************************************")
-                print("For Staus Code: " + str(json_status) + "; Refer to:")
-                print("https://developer.mapquest.com/documentation/directions-api/status-codes")
-                print("************************************************************************\n")
+                msg.set("SOMETHING WENT WROGN!")
+                lbl.pack()
 
 def get_input():
    global orig
@@ -52,8 +52,19 @@ def get_input():
    orig = source_txt.get(1.0, "end-1c")
    dest = destination_txt.get(1.0, "end-1c")
    gas = gas_txt.get(1.0, "end-1c")
-   get_direction()
-   Button(win, height=1, width=10, text="Close ", command=destroy)
+   if(orig and dest and gas):
+       if(gas.isdigit()):
+        get_direction()
+       else:
+        msg.set("GAS PRICE MUST BE INTEGER!")
+        lbl.pack()    
+   else:
+       msg.set("PLEASE FILL OUT THE FORM!")
+       lbl.pack()
+
+msg = StringVar()
+lbl = Label( win, textvariable=msg)
+
 
 def destroy():
         win.destroy()
