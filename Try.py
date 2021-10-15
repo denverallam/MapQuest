c697f836bd3c5f398f87c39de87a9c3eda98361f
denverallam <denver.allam.iics@ust.edu.ph>
2021-10-15 16:05:04 +0800

    fixed

--- Try.py
+++ Try.py
@@ -12,6 +12,7 @@ def get_input():
    global dest
    orig = source_txt.get(1.0, "end-1c")
    dest = destination_txt.get(1.0, "end-1c")
+   win.destroy()
 
 #Creating a text box widget for source
 source_lbl = Label( win, text="Source City")
@@ -33,25 +34,25 @@ navigate= Button(win, height=5, width=10, text="Navigate", command=get_input)
 #command=get_input() will wait for the key to press and displays the entered text
 navigate.pack()
 
-win.mainloop()
+win.mainloop() 
 
 
 
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "MhvbvH6lJAKgMu9wisKM5iSoZWOFFPQJ"    # You should use your own key 
-while True:
+
     # orig = input ("Source City :")
     # if orig == "quit" or orig == "q":
     #     break
     # dest = input("Dest City :")
     # if dest == "quit" or dest == "q":
     #     break
-    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
-    print ("URL ", (url))
-    json_data = requests.get(url).json()
-    json_status = json_data["info"]["statuscode"]
-    if json_status == 0:
+url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+print ("URL ", (url))
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+if json_status == 0:
         print ("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
         print("Directions from " + (orig) + " to " + (dest))
@@ -62,15 +63,15 @@ while True:
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
         print("=============================================\n")
-    elif json_status == 402:
+elif json_status == 402:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or bothlocations.")
         print("**********************************************\n")
-    elif json_status == 611:
+elif json_status == 611:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Missing an entry for one or bothlocations.")
         print("**********************************************\n")
-    else:
+else:
         print("************************************************************************")
         print("For Staus Code: " + str(json_status) + "; Refer to:")
         print("https://developer.mapquest.com/documentation/directions-api/status-codes")
